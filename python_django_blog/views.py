f46b647a6809c15a1fc26aad285380078952f1c7
DavidOBr <david.obrien.nord@gmail.com>
2024-02-20 18:44:11 +0000

    basic logic for when users are implemented
    wanted to create the logic, so when I am happy with the current front end I can begin to have users and they will be able to login/register if they're not logged in, if they are already logged in then I want to show them the dashboard link

--- python_django_blog/views.py
+++ python_django_blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'python_django_blog/home.html')
+    return render(request, 'python_django_blog/home.html', {'user': request.user})
